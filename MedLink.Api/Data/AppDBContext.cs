43d104e0e2cc135a1fd2347734f7f661279cf482
IrvineCaleb <caleb.irvine1107@omail.otech.edu>
2025-06-30 15:03:52 -0600

    Creation of patient model

---
+++ MedLink.Api/Data/AppDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MedLink.Logic.Models;
+
+namespace MedLink.Api.Data
+{
+    public class AppDBContext
+    {
+    }
+}
---
+++ MedLink.Logic/Models/Patient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedLink.Logic.Model
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }    
+        public DateTime createdAt { get; set; }
+        public DateTime lastModified {  get; set; }
+        public string patientNote { get; set; }
+    }
+}
