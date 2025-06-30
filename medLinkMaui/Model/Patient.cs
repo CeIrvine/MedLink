cfc1ead82e2271bc0f73f0af5483b3d231885294
IrvineCaleb <caleb.irvine1107@omail.otech.edu>
2025-06-30 14:29:52 -0600

    Local Database publish. Download of nugget packages

---
+++ medLinkMaui/Model/Patient.cs
@@ -0,0 +1,11 @@
+﻿namespace medLinkMaui.Model;
+
+public class Patient
+{
+    public int Id { get; set; }
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public DateTime lastModified { get; set; }
+    public DateTime createdAt {  get; set; }
+    public string patientNote { get; set; }
+}
