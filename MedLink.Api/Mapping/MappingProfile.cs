c9c584f592aba5cb783f7075cd32a30baef97796
IrvineCaleb <caleb.irvine1107@omail.otech.edu>
2025-07-02 11:44:04 -0600

    Fixed my MappingProfile. Post and Push were assigning database to ui instead of ui to cleint.

--- MedLink.Api/Mapping/MappingProfile.cs
+++ MedLink.Api/Mapping/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MedLink.Api.DTOs.Get;
 using MedLink.Api.DTOs.Post;
+using MedLink.Api.DTOs.Push;
 using MedLink.Logic.Models;
 
 namespace MedLink.Api.Mapping
@@ -12,19 +13,30 @@ namespace MedLink.Api.Mapping
             CreateMap<Biometrics, GetBiometricsDTO>();
             CreateMap<Doctor, GetDoctorDTO>();
             CreateMap<Illness, GetIllnessDTO>();
+            CreateMap<Insurance, GetInsuranceDTO>();
             CreateMap<Operation, GetOperationDTO>();
             CreateMap<Patient, GetPatientDTO>();
             CreateMap<User, GetUserDTO>();
             CreateMap<Visit, GetVisitDTO>();
 
-            CreateMap<Biometrics, PostBiometricsDTO>();
-            CreateMap<Doctor, PostDoctorDTO>();
-            CreateMap<Illness, PostIllnessDTO>();
-            CreateMap<Insurance, PostInsuranceDTO>();
-            CreateMap<Operation, PostOperationDTO>();
-            CreateMap<Patient, PostPatientDTO>();
-            CreateMap<User, PostUserDTO>();
-            CreateMap<Visit, PostVisitDTO>();
+            CreateMap<PostBiometricsDTO, Biometrics>();
+            CreateMap<PostDoctorDTO, Doctor>();
+            CreateMap<PostIllnessDTO, Illness>();
+            CreateMap<PostInsuranceDTO, Insurance>();
+            CreateMap<PostOperationDTO, Operation>();
+            CreateMap<PostPatientDTO, Patient>();
+            CreateMap<PostUserDTO, User>();
+            CreateMap<PostVisitDTO, Visit>();
+
+            CreateMap<PushBiometricsDTO, Biometrics>();
+            CreateMap<PushDoctorDTO, Doctor>();
+            CreateMap<PushIllnessDTO, Illness>();
+            CreateMap<PushInsuranceDTO, Insurance>();
+            CreateMap<PushOperationDTO, Operation>();
+            CreateMap<PushPatientDTO, Patient>();
+            CreateMap<PushUserDTO, User>();
+            CreateMap<PushVisitDTO, Visit>();
         }
     }
 }
+
